d8993ee7a496dd96dd0a3dbe5d5e7246eaba87c1
divyeshjethva <divyeshjethva2004@gmail.com>
2025-05-25 14:51:47 +0530

    String Manipulation

---
+++ 1. Python Fundamentals/practical/task9.py
@@ -0,0 +1,30 @@
+# String Manipulation
+
+# Write a Python program to demonstrate string slicing.
+
+# text = "Hello, Python!"
+
+# print("original string:", text)
+# print(text[0:5])
+# print(text[7:])
+# print(text[:5])
+# print(text[:])
+# print(text[-7:-1])
+# print(text[::2])
+
+# ============================================================
+
+#  Write a Python program that manipulates and prints strings using various string methods.
+
+# Demonstrate various string methods
+text = "  welcome to PYTHON programming!  "
+
+print(text.strip())
+print(text.lower())
+print(text.upper())
+print(text.replace("PYTHON", "Java"))
+print(text.find("programming"))
+print(text.count('o'))
+print(text.startswith("welcome"))
+print(text.endswith("!"))
+print(text.swapcase())
